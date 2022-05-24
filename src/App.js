6a090a8a2392680ddd5268e18f63ac8e121343cd
Liam Vlaskamp <lvlaskamp@gmail.com>
2022-05-24 13:13:36 +0200

    Initial header implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, createTheme, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Header />
+      </Box>
+    </ThemeProvider>
   );
 }
 
